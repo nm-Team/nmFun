2f785e28da235774b4271c0571fed049769c7749
agou <2833769658@qq.com>
2021-11-19 18:58:22 +0800

    [Fix] 解决了一些已知问题
    - 修正等待搜索时显示的照片的作图错误
    - 修正深色模式下页面选择器背景颜色错误的问题
    - 修正 frame.css 中页面颜色位置错误的问题
    - 正在对代码结构做出更改，以解决部分代码结构混乱的问题
    - 正在对代码结构做出更改，以解决部分代码不便于开发的问题

--- src/js/example.js
+++ src/js/example.js
@@ -2,38 +2,38 @@ new_element = document.createElement('object');
 new_element.innerHTML = `
 <div class="postMainReal card avatarBox">
     <div class="header">
-        <a class="name" tabindex="0" onclick="newUserInfoPage('{{uid}}');"
+        <a class="name" tabindex="0" onclick="newUserInfoPage('uid');"
             onkeydown="divClick(this, event)"><i
-                style="background-image:url('{{avatar}}')"></i>
+                style="background-image:url('avatar')"></i>
             <div>
-                <p class="unick">{{nick}}</p>
-                <p class="time" time="true" timestamp="{{time}}" timestyle="relative"
+                <p class="unick">nick</p>
+                <p class="time" time="true" timestamp="time" timestyle="relative"
                     timesec="false" timefull="false"
-                    title="null-null-null null:null:null">现在</p>
+                    title=""></p>
             </div>
         </a>
         <div class="buttons">
-            <button onclick="postContextMenu('{{type}}', '{{id}}', this);"><i
+            <button onclick="postContextMenu('type', 'id', this);"><i
                     class="material-icons">more_vert</i></button>
         </div>
     </div>
     <div class="content">
-        <p class="status">{{status}}</p>
-        <a href="" target="_blank" onclick="" class="text"><object class="{{slug}}">
-                <p>{{content}}</p>
+        <p class="status">status</p>
+        <a href="" target="_blank" onclick="" class="text"><object class="slug">
+                <p>content</p>
             </object></a>
         <!-- 注：预览页（而不是详情页）此处的设定是点content进入详情页 -->
         <!-- 同时设置onclick（优先）和a标签（新窗口打开，target=blank） -->
         <div class="media">
-            <ui class="medias" id="tes" type="x{{mediatype}}">{{medias}}</ui>
-            {{specialMedias}}
+            <ui class="medias" id="tes" type="xmediatype">medias</ui>
+            specialMedias
         </div>
     </div>
     <div class="bottom">
         <div class="word">
-            <p>浏览<span class="viewNum">{{view}}</span>次</p>
+            <p>浏览<span class="viewNum">view</span>次</p>
         </div>
-        <div class="buttons">{{buttons}} </div>
+        <div class="buttons">buttons </div>
     </div>
 </div>`;
 $("post").append(new_element);
