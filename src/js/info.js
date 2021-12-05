d64bd84886b61fae57c058af761317e65b6c8133
agoudbg <55237525+agoudbg@users.noreply.github.com>
2021-12-05 11:58:28 +0800

    [Fix] 修正 info 错误

--- src/js/info.js
+++ src/js/info.js
@@ -3,8 +3,8 @@ accountClient = "https://accounts.nmteam.xyz";
 categoryList = [];
 debugMode = true;
 version = {
-    version: "0.2_dev",
+    version: "0.2_beta",
     versionNum: "1",
-    branch: "co_refresh",
+    branch: "co_dev_user",
     betaVersion: true
 }
