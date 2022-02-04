b8a9d8498e266fa3c250db4cc45c331eb54aea29
agou <2833769658@qq.com>
2022-02-04 15:05:15 +0800

    [Fix] 解决了一些已知问题
    - 修正了一个代码逻辑错误，该错误导致信息流加载完成后加载完成提示不显示
    - 减小了信息流中加载完成提示和加载错误提示的高度
    - 为加载错误提示增加按钮“重试”

--- src/js/post_get.js
+++ src/js/post_get.js
@@ -3,7 +3,7 @@ function initPostsList(box, attr) {
     box[0].className += " postsList cardBox postCardBox ";
     box.attr("data-config", JSON.stringify(attr));
     box.attr("data-status", "undefined");
-    box.append(`<div class="main"></div><div class="mark"></div><div spe class="loading">${postSkeleton}</div><div spe class="card error">${postsErrorBoxHTML}</div><div spe class="card nomore">${postsNoMoreBoxHTML}</div>`);
+    box.append(`<div class="main"></div><div class="mark"></div><div spe class="loading">${postSkeleton}</div><div spe class="card error">${postsErrorBoxHTML.replace(/{boxid}/g,box[0].id)}</div><div spe class="card nomore">${postsNoMoreBoxHTML}</div>`);
     writeLog("i", "initPostsList", `box id: ${box[0].id},attr: ${JSON.stringify(attr)}`);
 }
 
@@ -96,15 +96,15 @@ function loadPostsList(box) {
                 <svg class="comment" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <path d="M853.333333 768c35.413333 0 64-20.650667 64-55.978667V170.581333A63.978667 63.978667 0 0 0 853.333333 106.666667H170.666667C135.253333 106.666667 106.666667 135.253333 106.666667 170.581333v541.44C106.666667 747.285333 135.338667 768 170.666667 768h201.173333l110.016 117.44a42.752 42.752 0 0 0 60.586667 0.042667L651.904 768H853.333333z m-219.029333-42.666667h-6.250667l-115.797333 129.962667c-0.106667 0.106667-116.010667-129.962667-116.010667-129.962667H170.666667c-11.776 0-21.333333-1.621333-21.333334-13.312V170.581333A21.205333 21.205333 0 0 1 170.666667 149.333333h682.666666c11.776 0 21.333333 9.536 21.333334 21.248v541.44c0 11.754667-9.472 13.312-21.333334 13.312H634.304zM341.333333 490.666667a42.666667 42.666667 0 1 0 0-85.333334 42.666667 42.666667 0 0 0 0 85.333334z m170.666667 0a42.666667 42.666667 0 1 0 0-85.333334 42.666667 42.666667 0 0 0 0 85.333334z m170.666667 0a42.666667 42.666667 0 1 0 0-85.333334 42.666667 42.666667 0 0 0 0 85.333334z" p-id="5116"></path>
                 </svg>
-                <span class="commentNum">0</span>
+                <span class="commentNum">${info.comment}</span>
             </button>
         </div>
     </div>
 </div>`;
                     box.children(".main").append(new_element);
-                    box.attr("data-status", "undefined");
-                    if (response['data'] == []) box.attr("data-status", "nomore");
                 });
+                if (response['data'].length == 0) box.attr("data-status", "nomore");
+                else box.attr("data-status", "undefined");
             }
             catch (err) {
                 writeLog("e", "loadPostsList", err);
@@ -150,6 +150,6 @@ postSkeleton = `
 </div>`;
 
 postsNoMoreBoxHTML = `
-<div class="unavaliable" noselect=""><i></i><p>没有了，怎么想都没有了吧！</p></div>`;
+<div class="unavaliable small" noselect=""><i></i><p>没有了，怎么想都没有了吧！</p></div>`;
 postsErrorBoxHTML = `
-<div class="unavaliable search-not-start" noselect=""><i></i><p>坏掉了啦，载不出来了！</p></div>`;
+<div class="unavaliable small search-not-start" noselect=""><i></i><p>坏掉了啦，载不出来了！<br><button onclick="$('#{boxid}').attr('data-status','undefined');loadPostsList($('#{boxid}'));">重试</button></p></div>`;
