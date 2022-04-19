cff96ffbe823557f94c61c181c65a80fa8d22d2a
agoudbg <2833769658@qq.com>
2022-04-20 01:17:22 +0800

    [Fix] 解决了一些已知问题
    - 测试对手机上输入框挤压附件栏问题的修复

--- src/js/info.js
+++ src/js/info.js
@@ -8,7 +8,7 @@ debugMode = true;
 systemCategoryList = [{ "id": "focus", "name": "关注" }, { "id": "", "name": "全部" }, { "id": "selected", "name": "精选" }, { "id": "hot", "name": "热榜" }]
 version = {
     version: "0.3_dev",
-    versionNum: "99.65.1",
+    versionNum: "99.65.2",
     branch: "co_refresh",
     betaVersion: true
 }
